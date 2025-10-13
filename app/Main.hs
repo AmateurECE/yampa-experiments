59be75a28fd614ff69a1f4b9601b1fc9fd73c42d
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-10-13 18:21:45 -0500

    activation-switch: Signal functions for the controller

--- app/Main.hs
+++ app/Main.hs
@@ -1,48 +1,104 @@
+{-# LANGUAGE Arrows #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
 
-import Control.Concurrent
 import Control.Monad
 import qualified Data.ByteString as BS
 import Data.Char (chr)
 import Data.IORef
+import Data.List (intercalate)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
+import Data.Time
 import Data.Word
-import Foreign.Marshal
-import Numeric
-import System.Exit (exitSuccess)
+import FRP.Yampa hiding (after, count, event, now)
+import Foreign
 import System.IO
 import System.Posix
 import Text.Regex.TDFA hiding (after, match)
 
 data SwitchState = Unpressed | Pressed deriving (Show)
 
-data KeyEvent = KeyEvent
-  { id :: Char,
+data KeyState = KeyState
+  { keyId :: Char,
     state :: SwitchState
   }
   deriving (Show)
 
--- NOTE: Happy paths first
-parseInput :: [Char] -> [KeyEvent]
-parseInput bytes = case bytes of
-  ('\x1B' : '[' : rest) -> parseKeyEvent rest
-  (key : rest) -> KeyEvent key Pressed : parseInput rest
+toString :: [Word8] -> String
+toString = T.unpack . E.decodeUtf8 . BS.pack
+
+printKeyStates :: [KeyState] -> String
+printKeyStates states = intercalate ", " $ fmap (\KeyState {keyId, state} -> (show keyId) ++ ": " ++ (show state)) states
+
+-- Parse input from terminal emulator as a KeyEvent
+parseKeyEvents :: [Char] -> [KeyState]
+parseKeyEvents bytes = case bytes of
+  ('\x1B' : '[' : rest) -> parseUnpressed rest
+  (key : rest) -> KeyState key Pressed : parseKeyEvents rest
   [] -> []
   where
-    parseKeyEvent event =
+    parseUnpressed event =
       let (_, _, after, groups) = event =~ "([0-9]+);[0-9]:3u" :: (String, String, String, [String])
        in case groups of
-            [key] -> KeyEvent (chr $ read key) Unpressed : parseInput after
-            _ -> parseInput event
+            [key] -> KeyState (chr $ read key) Unpressed : parseKeyEvents after
+            _ -> parseKeyEvents event
+
+initialize :: IO (Event [a])
+initialize = pure NoEvent
+
+sense :: Ptr Word8 -> Int -> IORef UTCTime -> Bool -> IO (DTime, Maybe (Event [Word8]))
+sense buffer size lastTimeRef _ = do
+  count <- hGetBufNonBlocking stdin buffer size
+
+  now <- getCurrentTime
+  lastTime <- readIORef lastTimeRef
+  writeIORef lastTimeRef now
+  let dt = realToFrac (now `diffUTCTime` lastTime)
+
+  if count > 0
+    then do
+      bytes <- peekArray count buffer :: IO [Word8]
+      return (dt, Just $ Event bytes)
+    else return (dt, Just NoEvent)
+
+actuate :: IORef Bool -> Bool -> [KeyState] -> IO Bool
+actuate terminate hasChanged value = do
+  when hasChanged $ do
+    putStr $ "\r" ++ (printKeyStates value) ++ "\ESC[0K"
+    hFlush stdout
+  readIORef terminate >>= return
+
+--
+-- Signal Functions
+--
+
+fanOutEvents :: Event [a] -> [Event a]
+fanOutEvents (Event items) = map Event items
+fanOutEvents NoEvent = []
+
+parseKeyEventsSF :: SF (Event [Word8]) ([Event KeyState])
+parseKeyEventsSF = arr $ fanOutEvents . fmap toEvents
+  where
+    toEvents = parseKeyEvents . toString
+
+keySF :: Char -> SF ([Event KeyState]) KeyState
+keySF target = hold (KeyState target Unpressed) <<< (arr $ mergeEvents) <<< filterByKey
+  where
+    filterByKey = arr (fmap $ filterE (\e -> keyId e == target)) :: SF ([Event KeyState]) ([Event KeyState])
+
+application :: SF (Event [Word8]) [KeyState]
+application = arr (\(a, (b, c)) -> [a, b, c]) <<< ((keySF '1') &&& (keySF '2') &&& (keySF '3')) <<< parseKeyEventsSF
 
 main :: IO ()
 main = do
+  -- Terminate gracefully when Ctrl-C is pressed
   terminate <- newIORef False
-  _ <- installHandler sigINT (Catch $ exit terminate) Nothing
+  let exit = writeIORef terminate True
+  _ <- installHandler sigINT (Catch exit) Nothing
 
   hSetBuffering stdin NoBuffering
   hSetEcho stdin False
@@ -54,22 +110,10 @@ main = do
   let size = 32
   buf <- mallocBytes size
 
-  let cleanup = do
-        -- Restore the terminal mode
-        putStr "\x1B[<2u"
-        hFlush stdout
-        free buf
-        putStrLn "Exiting gracefully..."
-        exitSuccess
+  lastTimeRef <- getCurrentTime >>= newIORef
+  reactimate initialize (sense buf size lastTimeRef) (actuate terminate) application
 
-  forever $ do
-    threadDelay 100000 -- 0.1s
-    count <- hGetBufNonBlocking stdin buf size
-    when (count > 0) $ do
-      bytes <- peekArray count buf :: IO [Word8]
-      putStrLn $ show $ parseInput $ T.unpack $ E.decodeUtf8 $ BS.pack bytes
-    shouldTerminate <- readIORef terminate
-    when shouldTerminate cleanup
-
-exit :: IORef Bool -> IO ()
-exit signal = writeIORef signal True
+  -- Restore the terminal mode
+  putStrLn "\x1B[<2u"
+  hFlush stdout
+  free buf
