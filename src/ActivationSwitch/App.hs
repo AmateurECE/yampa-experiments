28c8d9d1523cb651fe13027119918b888d864490
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-10-20 07:18:19 -0500

    activation-switch: Code cleanup in App

--- src/ActivationSwitch/App.hs
+++ src/ActivationSwitch/App.hs
@@ -4,42 +4,44 @@ module ActivationSwitch.App
   ( application,
     initialize,
     renderUI,
-    TherapyState,
+    T.TherapyState,
   )
 where
 
-import ActivationSwitch.Switch
-import ActivationSwitch.Therapy
+import qualified ActivationSwitch.Switch as S
+import qualified ActivationSwitch.Therapy as T
 import qualified Data.ByteString as BS
+import Data.Foldable
 import FRP.Yampa
+import Linear
 
 initialize :: IO (Event a)
 initialize = pure NoEvent
 
-keySF :: Char -> SF ([Event KeyState]) KeyState
+keySF :: Char -> SF ([Event T.KeyState]) T.KeyState
 keySF target = proc allKeys -> do
   key <- filterByKey -< allKeys
   current <- arr $ mergeEvents -< key
-  stable <- hold (KeyState target Unpressed) -< current
+  stable <- hold (T.KeyState target T.Unpressed) -< current
   returnA -< stable
   where
-    filterByKey = arr (fmap $ filterE (\e -> keyId e == target))
+    filterByKey = arr (fmap $ filterE (\e -> T.keyId e == target))
 
-controller :: SF ([Event KeyState]) [KeyState]
-controller = proc events -> do
+controllerSF :: SF ([Event T.KeyState]) (V3 T.KeyState)
+controllerSF = proc events -> do
   one <- keySF '1' -< events
   two <- keySF '2' -< events
   three <- keySF '3' -< events
-  returnA -< [one, two, three]
+  returnA -< V3 one two three
 
-therapySF :: SF [KeyState] TherapyState
+therapySF :: SF (V3 T.KeyState) T.TherapyState
 therapySF = proc keyStates -> do
-  rec let current = therapy previous keyStates
-      previous <- iPre Inactive -< current
+  rec let current = T.therapy previous $ toList keyStates
+      previous <- iPre T.Inactive -< current
   returnA -< current
 
-application :: SF (Event BS.ByteString) TherapyState
-application = therapySF <<< controller <<< parseKeyEventsSF
+application :: SF (Event BS.ByteString) T.TherapyState
+application = therapySF <<< controllerSF <<< S.parseKeyEventsSF
 
-renderUI :: TherapyState -> IO ()
+renderUI :: T.TherapyState -> IO ()
 renderUI therapyState = putStr $ "\rTherapy: " ++ show therapyState ++ "\ESC[0K"
